285ece9d66a8b7d96406e939c801912206de0dbc
mfaizk <faiz-react@tarality.com>
2025-08-04 17:56:15 +0530

    sitemap and robot text added

---
+++ sitemap.js
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://nowory.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://nowory.com/terms/termsConditions",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://nowory.com/terms/amlPolicy",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://nowory.com/terms/privacyPolicy",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://nowory.com/terms/tradingPolicy",
+      lastModified: new Date(),
+    },
+  ];
+}
